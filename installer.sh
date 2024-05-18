03585a1c91cad189038c8333d83d17fd7f7055c6
FonWasH <fonwash@gmail.com>
2024-05-18 22:29:39 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -27,6 +27,7 @@ ZSHInstall()
 
 	if $zsh_install; then
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 		clear
 		echo "ZSH and Oh My ZSH installed."
 	else
